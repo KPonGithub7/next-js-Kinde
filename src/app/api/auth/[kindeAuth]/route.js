5351df9b935814cdedeefd366d5239245150089b
Kunal Patel <kunalpatel88843@gmail.com>
2024-07-22 20:46:14 +0530

    fix: build error

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 export const GET = handleAuth();
 
-export default function handler(req, res) {
-    const authorizationUrl = `https://kunalpatel.kinde.com/oauth2/authorize?client_id=${process.env.KINDE_CLIENT_ID}&redirect_uri=${process.env.KINDE_REDIRECT_URI}&response_type=code&scope=openid profile email`;
+// export default function handler(req, res) {
+//     const authorizationUrl = `https://kunalpatel.kinde.com/oauth2/authorize?client_id=${process.env.KINDE_CLIENT_ID}&redirect_uri=${process.env.KINDE_REDIRECT_URI}&response_type=code&scope=openid profile email`;
 
-    res.redirect(authorizationUrl);
-}
+//     res.redirect(authorizationUrl);
+// }
